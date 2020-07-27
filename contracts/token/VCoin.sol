3418d3c70263432709db63b0bf0ad1faf4040a76
George Spasov <spasov05203@gmail.com>
2020-07-27 20:14:45 +0300

    Renamed VCoin to VCOIN

--- contracts/token/VCoin.sol
+++ contracts/token/VCoin.sol
@@ -3,5 +3,5 @@ pragma solidity >=0.6.7 <0.7.0;// SPDX-License-Identifier: MIT
 import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
 
 contract VCoin is ERC20PresetMinterPauser {
-    constructor() public ERC20PresetMinterPauser("VCoin", "VCO") {}
+    constructor() public ERC20PresetMinterPauser("VCOIN", "VCO") {}
 }
